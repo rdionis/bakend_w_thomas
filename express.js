0002680219aa773fca4e797dee1a5939e909529b
Raquel Dionísio <dionisio.raquel@gmail.com>
2021-05-28 16:41:41 +0200

    using axios

--- express.js
+++ express.js
@@ -1,17 +1,33 @@
 const express = require("express");
+const axios = require("axios");
 const app = express(); //this is creating a server
 const port = 5000;
 
 app.get("/", (req, res) => {
   console.log("This is our first express class");
-  res.write("<h1>First</h1>");
-  res.end("Goodbye");
+  res.write("<h1>Hello world</h1>");
+  res.end();
 });
 
-app.get("/last", (req, res) => {
-  console.log("Last example for today");
-  res.write("<h1>Last</h1>");
+// REST Architecture
+//users/1/texts/4/paragraph/2
+app.get("/users", (req, res) => {
   res.end();
+});
+// variable in url :id
+app.get("/users/:id", (req, res) => {
+  //1. receive ID over URL
+  const userId = req.params.id;
+  //2. request external endpoint/resource
+  axios
+    //axios.get returns PROMISE because the request takes a few ms
+    .get("https://jsonplaceholder.typicode.com/users/" + userId)
+    // wait for the PROMISE with then
+    .then(function (response) {
+      //3. handle the response
+      console.log(response.data);
+      res.json(response.data);
+    });
 });
 
 app.listen(port, () => {
